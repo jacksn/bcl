4445d6c36b5e95f865c5eba50c7d6ff28971917d
Eugene Akhramovich <EugeneAkhramovich@gmail.com>
2017-10-17 13:34:37 +0300

    Add base entity

---
+++ app/src/main/java/ru/atc/bclient/model/BaseEntity.java
@@ -0,0 +1,48 @@
+package ru.atc.bclient.model;
+
+import javax.persistence.*;
+
+@MappedSuperclass
+// http://stackoverflow.com/questions/594597/hibernate-annotations-which-is-better-field-or-property-access
+@Access(AccessType.FIELD)
+public abstract class BaseEntity {
+    @Id
+    // PROPERTY access for id due to bug: https://hibernate.atlassian.net/browse/HHH-3718
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Access(value = AccessType.PROPERTY)
+    private Integer id;
+
+    public BaseEntity() {
+    }
+
+    public BaseEntity(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public boolean isNew() {
+        return id == null;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        BaseEntity that = (BaseEntity) o;
+
+        return id != null ? id.equals(that.id) : that.id == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
+}
