413bd36dc0a3cb30eae73581b45f71cd028f33d5
Eugene Akhramovich <EugeneAkhramovich@gmail.com>
2017-10-17 19:11:14 +0300

    Update data access configuration

--- app/src/main/java/ru/atc/bclient/config/DataAccessConfiguration.java
+++ app/src/main/java/ru/atc/bclient/config/DataAccessConfiguration.java
@@ -1,13 +1,18 @@
 package ru.atc.bclient.config;
 
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
-@PropertySource("classpath:db/db.properties")
+@PropertySources({
+        @PropertySource("classpath:db/db.properties"),
+        @PropertySource(value = "classpath:db/${db_config_file}", ignoreResourceNotFound = true)})
 @EnableTransactionManagement
+@EntityScan("ru.atc.bclient.model")
 @EnableJpaRepositories(basePackages = "ru.atc.bclient.repository")
 public class DataAccessConfiguration {
 }
