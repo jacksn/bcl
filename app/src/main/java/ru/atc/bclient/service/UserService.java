bb98fb823f2e079bcc580b95ff1a88f02c1fc5b7
Eugene Akhramovich <EugeneAkhramovich@gmail.com>
2017-10-18 21:47:32 +0300

    Add User service

---
+++ app/src/main/java/ru/atc/bclient/service/UserService.java
@@ -0,0 +1,4 @@
+package ru.atc.bclient.service;
+
+public interface UserService {
+}
---
+++ app/src/main/java/ru/atc/bclient/service/UserServiceImpl.java
@@ -0,0 +1,28 @@
+package ru.atc.bclient.service;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import ru.atc.bclient.model.User;
+import ru.atc.bclient.repository.UserRepository;
+import ru.atc.bclient.security.AuthorizedUser;
+
+@Service
+public class UserServiceImpl implements UserService, UserDetailsService {
+
+    private UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String login) throws UsernameNotFoundException {
+        User user = userRepository.findByLogin(login);
+        if (user == null) {
+            throw new UsernameNotFoundException("Пользователь с логином " + login + " не найден.");
+        }
+        return new AuthorizedUser(user);
+    }
+}
