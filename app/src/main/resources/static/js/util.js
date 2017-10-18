37c67bce722dde39ea892154ebbb244168b97d3f
Eugene Akhramovich <EugeneAkhramovich@gmail.com>
2017-10-18 14:30:03 +0300

    Add static content

---
+++ app/src/main/resources/static/js/util.js
@@ -0,0 +1,21 @@
+function addErrorHighlight(field) {
+    $(field).parent().addClass("has-error");
+}
+
+function removeErrorHighlight(field) {
+    $(field).parent().removeClass('has-error');
+}
+
+function showNotification(type, message) {
+    $.notify({
+            message: message
+        }, {
+            type: type,
+            placement: {
+                from: "bottom",
+                align: "right"
+            },
+            mouse_over: "pause"
+        }
+    );
+}
