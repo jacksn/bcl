fb5e5958de7b46aea82b5a523375a25e63ac57b7
Eugene Akhramovich <EugeneAkhramovich@gmail.com>
2017-10-18 21:48:52 +0300

    Add Spring Security support

---
+++ app/src/main/java/ru/atc/bclient/config/SecurityConfig.java
@@ -0,0 +1,36 @@
+package ru.atc.bclient.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/static/**").permitAll()
+                .antMatchers("/payment/**").authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .defaultSuccessUrl("/")
+                .failureForwardUrl("/login-error")
+                .and()
+                .logout()
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .logoutSuccessUrl("/login");
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth,
+                                UserDetailsService userDetailsService) throws Exception {
+        auth.userDetailsService(userDetailsService);
+    }
+}
---
+++ app/src/main/java/ru/atc/bclient/security/AuthorizedUser.java
@@ -0,0 +1,33 @@
+package ru.atc.bclient.security;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import ru.atc.bclient.model.User;
+
+import java.util.Collections;
+
+import static java.util.Objects.requireNonNull;
+
+public class AuthorizedUser extends org.springframework.security.core.userdetails.User {
+    public AuthorizedUser(User user) {
+        super(user.getLogin(),
+                user.getPassword(),
+                Collections.singletonList(new SimpleGrantedAuthority("USER")));
+    }
+
+    public static AuthorizedUser safeGet() {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth == null) {
+            return null;
+        }
+        Object principal = auth.getPrincipal();
+        return (principal instanceof AuthorizedUser) ? (AuthorizedUser) principal : null;
+    }
+
+    public static AuthorizedUser get() {
+        AuthorizedUser user = safeGet();
+        requireNonNull(user, "No authorized user found");
+        return user;
+    }
+}
