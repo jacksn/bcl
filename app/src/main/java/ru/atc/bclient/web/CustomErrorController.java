7d206609ad62dd24ab5aea50d4d6c017d3bfd9d0
Eugene Akhramovich <EugeneAkhramovich@gmail.com>
2017-10-18 18:10:20 +0300

    Add error controller and view

---
+++ app/src/main/java/ru/atc/bclient/web/CustomErrorController.java
@@ -0,0 +1,32 @@
+package ru.atc.bclient.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.web.ErrorAttributes;
+import org.springframework.boot.autoconfigure.web.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class CustomErrorController implements ErrorController {
+    private static final String PATH = "/error";
+
+    @Autowired
+    private ErrorAttributes errorAttributes;
+
+    @RequestMapping(PATH)
+    public String error(Model model, HttpServletRequest request) {
+        RequestAttributes requestAttributes = new ServletRequestAttributes(request);
+        model.addAllAttributes(this.errorAttributes.getErrorAttributes(requestAttributes, false));
+        return "error";
+    }
+
+    @Override
+    public String getErrorPath() {
+        return PATH;
+    }
+}
