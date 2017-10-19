bcbd5b3a7864ea6a9cd130bc480db4a8b2d8d5cc
Eugene Akhramovich <EugeneAkhramovich@gmail.com>
2017-10-19 22:45:14 +0300

    Update tests

--- app/src/test/java/ru/atc/bclient/repository/UserRepositoryTest.java
+++ app/src/test/java/ru/atc/bclient/repository/UserRepositoryTest.java
@@ -2,15 +2,8 @@ package ru.atc.bclient.repository;
 
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import ru.atc.bclient.model.Account;
-import ru.atc.bclient.model.LegalEntity;
-import ru.atc.bclient.model.User;
-
-import java.util.Iterator;
-import java.util.Set;
 
 import static org.junit.Assert.assertEquals;
-import static ru.atc.bclient.LegalEntityTestData.LEGAL_ENTITY_1;
 import static ru.atc.bclient.UserTestData.USERS;
 import static ru.atc.bclient.UserTestData.USER_1;
 
@@ -26,23 +19,5 @@ public class UserRepositoryTest extends AbstractRepositoryTest {
     @Test
     public void findAll() {
         assertEquals(USERS, repository.findAll());
-    }
-
-    @Test
-    public void findWithLegalEntitiesById() {
-        User user = repository.findWithLegalEntitiesById(USER_1.getId());
-        assertEquals(USER_1, user);
-        assertEquals(USER_1.getLegalEntities(), user.getLegalEntities());
-    }
-
-    @Test
-    public void findWithLegalEntitiesAndAccountsById() {
-        User user = repository.findWithLegalEntitiesAndAccountsById(USER_1.getId());
-        assertEquals(USER_1, user);
-        assertEquals(USER_1.getLegalEntities(), user.getLegalEntities());
-
-        Iterator<LegalEntity> legalEntityIterator = user.getLegalEntities().iterator();
-        Set<Account> accounts = legalEntityIterator.next().getAccounts();
-        assertEquals(LEGAL_ENTITY_1.getAccounts(), accounts);
     }
 }
