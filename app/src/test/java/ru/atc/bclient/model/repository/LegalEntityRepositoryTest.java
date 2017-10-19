e4b395d8de9a259d1d3826cd80f5d124318e6724
Eugene Akhramovich <EugeneAkhramovich@gmail.com>
2017-10-19 23:16:55 +0300

    Update tests

--- app/src/test/java/ru/atc/bclient/model/repository/LegalEntityRepositoryTest.java
+++ app/src/test/java/ru/atc/bclient/model/repository/LegalEntityRepositoryTest.java
@@ -8,7 +8,6 @@ import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 import static ru.atc.bclient.LegalEntityTestData.LEGAL_ENTITIES;
-import static ru.atc.bclient.UserTestData.USER_1;
 
 public class LegalEntityRepositoryTest extends AbstractRepositoryTest {
     @Autowired
@@ -19,11 +18,4 @@ public class LegalEntityRepositoryTest extends AbstractRepositoryTest {
         List<LegalEntity> legalEntities = repository.findAll();
         assertEquals(LEGAL_ENTITIES, legalEntities);
     }
-
-    @Test
-    public void getByUserId() {
-        List<LegalEntity> legalEntities = repository.getByUserId(USER_1.getId());
-        legalEntities.forEach(System.out::println);
-    }
-
 }
