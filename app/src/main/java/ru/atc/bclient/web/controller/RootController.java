7ce6c0feb40b0c3db4bbe9b0610cd95d177fcf44
Eugene Akhramovich <EugeneAkhramovich@gmail.com>
2017-10-20 14:39:02 +0300

    Update root controller

--- app/src/main/java/ru/atc/bclient/web/controller/RootController.java
+++ app/src/main/java/ru/atc/bclient/web/controller/RootController.java
@@ -1,9 +1,11 @@
 package ru.atc.bclient.web.controller;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import ru.atc.bclient.web.security.AuthorizedUser;
 import ru.atc.bclient.web.to.Notification;
 import ru.atc.bclient.web.to.NotificationType;
 
@@ -13,8 +15,9 @@ public class RootController {
     public static final String NOTIFICATION = "notification";
 
     @GetMapping
-    public String root() {
-        return "redirect:/payment";
+    public String root(Model model, @AuthenticationPrincipal AuthorizedUser authorizedUser) {
+        model.addAttribute("legalEntities", authorizedUser.getLegalEntities());
+        return "userLegalEntities";
     }
 
     @RequestMapping("login")
