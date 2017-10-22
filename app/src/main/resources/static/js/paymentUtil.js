51db6d8b98217c8e092016530a2070de65fa2f55
Eugene Akhramovich <EugeneAkhramovich@gmail.com>
2017-10-22 20:02:34 +0300

    Update views: add filtering by legal entity

--- app/src/main/resources/static/js/paymentUtil.js
+++
@@ -1,9 +0,0 @@
-function filterEntries(parentName, entryName, id) {
-    $('[id=' + parentName + ']').find('[id^=' + entryName + '_]').each(function () {
-        if (id === 'ALL' || this.id === entryName + '_' + id) {
-            this.style.display = 'block';
-        } else {
-            this.style.display = 'none';
-        }
-    });
-}
--- app/src/main/resources/static/js/util.js
+++ app/src/main/resources/static/js/util.js
@@ -19,3 +19,13 @@ function showNotification(type, message) {
         }
     );
 }
+
+function filterEntries(parentName, entryName, id) {
+    $('[id=' + parentName + ']').find('[id^=' + entryName + '_]').each(function () {
+        if (id === 'ALL' || this.id === entryName + '_' + id) {
+            this.style.display = 'block';
+        } else {
+            this.style.display = 'none';
+        }
+    });
+}
