bdbe2277aea750d745cf45620e88f7fbe3105dc5
Eugene Akhramovich <EugeneAkhramovich@gmail.com>
2017-10-24 18:43:44 +0300

    Add support of Java 8 Time API to Thymeleaf

--- app/src/main/java/ru/atc/bclient/config/WebMvcConfiguration.java
+++ app/src/main/java/ru/atc/bclient/config/WebMvcConfiguration.java
@@ -1,9 +1,11 @@
 package ru.atc.bclient.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.thymeleaf.extras.java8time.dialect.Java8TimeDialect;
 
 @Configuration
 @EnableWebMvc
@@ -11,5 +13,10 @@ public class WebMvcConfiguration extends WebMvcConfigurerAdapter {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+    }
+
+    @Bean
+    public Java8TimeDialect java8TimeDialect() {
+        return new Java8TimeDialect();
     }
 }
