315ee2f06faba8ac3b4fd9a039fe845188fdaa32
Eugene Akhramovich <EugeneAkhramovich@gmail.com>
2017-10-31 20:01:35 +0300

    Refactoring and optimizations

--- app/src/main/java/ru/atc/bclient/service/impl/PaymentOrderProcessorImpl.java
+++ app/src/main/java/ru/atc/bclient/service/impl/PaymentOrderProcessorImpl.java
@@ -4,11 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import ru.atc.bclient.model.entity.AccountBalance;
-import ru.atc.bclient.model.entity.AccountStatus;
-import ru.atc.bclient.model.entity.Operation;
-import ru.atc.bclient.model.entity.PaymentOrder;
-import ru.atc.bclient.model.entity.PaymentOrderStatus;
+import ru.atc.bclient.model.entity.*;
 import ru.atc.bclient.model.repository.AccountBalanceRepository;
 import ru.atc.bclient.model.repository.OperationRepository;
 import ru.atc.bclient.model.repository.PaymentOrderRepository;
@@ -16,7 +12,7 @@ import ru.atc.bclient.service.PaymentOrderProcessor;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @Slf4j
@@ -50,36 +46,70 @@ public class PaymentOrderProcessorImpl implements PaymentOrderProcessor {
     @Override
     @Transactional
     public void process() {
-        setProcessingInProgress(true);
-        log.info("Processing start");
-        try {
-            LocalDate currentDate = LocalDate.now();
-            List<PaymentOrder> paymentOrdersToProcess =
-                    paymentOrderRepository.getAllByDateBetweenOrderByPriorityCodeAscDateAscIdAsc(currentDate, currentDate).stream()
-                            .filter(paymentOrder -> paymentOrder.getStatus().equals(PaymentOrderStatus.ACCEPTED))
-                            .collect(Collectors.toList());
+        if (!isProcessingInProgress()) {
+            setProcessingInProgress(true);
+            log.info("Processing start");
+            try {
+                LocalDate currentDate = LocalDate.now();
+                List<PaymentOrder> paymentOrdersToProcess =
+                        paymentOrderRepository.getAllByDateBetweenOrderByPriorityCodeAscDateAscIdAsc(currentDate, currentDate).stream()
+                                .filter(paymentOrder -> paymentOrder.getStatus().equals(PaymentOrderStatus.ACCEPTED))
+                                .collect(Collectors.toCollection(LinkedList::new));
 
-            for (PaymentOrder paymentOrder : paymentOrdersToProcess) {
-                paymentOrder.setStatus(PaymentOrderStatus.IN_PROGRESS);
-                if (isPaymentOrderValid(paymentOrder)) {
-                    createOperation(paymentOrder);
+                Map<Integer, AccountBalance> accountBalanceMap = new HashMap<>();
+                List<Operation> operations = new LinkedList<>();
 
-                    BigDecimal amount = paymentOrder.getAmount();
-                    updateAccountBalance(currentDate, paymentOrder.getSenderAccount().getId(), amount.negate());
-                    updateAccountBalance(currentDate, paymentOrder.getRecipientAccount().getId(), amount);
+                int processedPaymentOrders;
 
-                    paymentOrder.setStatus(PaymentOrderStatus.EXECUTED);
-                } else {
+                do {
+                    Iterator<PaymentOrder> iterator = paymentOrdersToProcess.iterator();
+                    processedPaymentOrders = 0;
+                    while (iterator.hasNext()) {
+                        PaymentOrder paymentOrder = iterator.next();
+                        paymentOrder.setStatus(PaymentOrderStatus.IN_PROGRESS);
+
+                        if (isPaymentOrderValid(paymentOrder)) {
+                            AccountBalance senderAccountBalance =
+                                    getAccountBalance(currentDate, paymentOrder.getSenderAccount().getId(), accountBalanceMap);
+                            if (senderAccountBalance.getAmount().compareTo(paymentOrder.getAmount()) > 0) {
+                                AccountBalance recipientAccountBalance =
+                                        getAccountBalance(currentDate, paymentOrder.getRecipientAccount().getId(), accountBalanceMap);
+
+                                operations.add(getOperation(paymentOrder));
+
+                                BigDecimal amount = paymentOrder.getAmount();
+
+                                senderAccountBalance.setAmount(senderAccountBalance.getAmount().add(amount.negate()));
+                                recipientAccountBalance.setAmount(recipientAccountBalance.getAmount().add(amount));
+
+                                paymentOrder.setStatus(PaymentOrderStatus.EXECUTED);
+                                iterator.remove();
+                                processedPaymentOrders++;
+                            }
+                        } else {
+                            iterator.remove();
+                            processedPaymentOrders++;
+                            paymentOrder.setStatus(PaymentOrderStatus.REJECTED);
+                        }
+                        paymentOrderRepository.save(paymentOrder);
+                    }
+                } while (paymentOrdersToProcess.size() > 0 && processedPaymentOrders > 0);
+
+                for (PaymentOrder paymentOrder : paymentOrdersToProcess) {
                     paymentOrder.setStatus(PaymentOrderStatus.REJECTED);
+                    paymentOrder.setRejectReason("На счете отправителя недостаточно средств.");
+                    paymentOrderRepository.save(paymentOrder);
                 }
-                paymentOrderRepository.save(paymentOrder);
+                operationRepository.save(operations);
+                accountBalanceRepository.save(accountBalanceMap.values());
+            } catch (Exception e) {
+                log.error("Error processing payment orders", e);
+                throw e;
+            } finally {
+                log.info("Processing end");
+                setProcessingInProgress(false);
             }
-        } catch (Exception e) {
-            log.error("Error processing payment orders", e);
-            throw e;
         }
-        log.info("Processing end");
-        setProcessingInProgress(false);
     }
 
     private boolean isPaymentOrderValid(PaymentOrder paymentOrder) {
@@ -89,32 +119,31 @@ public class PaymentOrderProcessorImpl implements PaymentOrderProcessor {
         } else if (!paymentOrder.getRecipientAccount().getStatus().equals(AccountStatus.ACTIVE)) {
             paymentOrder.setRejectReason("Счет получателя не активен.");
             return false;
-        } else if (paymentOrder.getAmount()
-                .compareTo(accountBalanceRepository.getFirstByAccountIdOrderByDateDesc(paymentOrder.getSenderAccount().getId())
-                        .getAmount()) > 0) {
-            paymentOrder.setRejectReason("На счете отправителя недостаточно средств.");
-            return false;
         }
         return true;
     }
 
-    private void updateAccountBalance(LocalDate date, Integer accountId, BigDecimal amount) {
-        AccountBalance accountBalance =
-                accountBalanceRepository.getFirstByAccountIdOrderByDateDesc(accountId);
-        if (!accountBalance.getDate().isEqual(date)) {
-            accountBalance = new AccountBalance(date, accountBalance.getAmount(), accountBalance.getAccount());
+    private AccountBalance getAccountBalance(LocalDate date, Integer accountId, Map<Integer, AccountBalance> accountBalanceMap) {
+
+        AccountBalance accountBalance = accountBalanceMap.get(accountId);
+
+        if (accountBalance == null) {
+            accountBalance = accountBalanceRepository.getFirstByAccountIdOrderByDateDesc(accountId);
+            if (!date.isEqual(accountBalance.getDate())) {
+                accountBalance = new AccountBalance(date, accountBalance.getAmount(), accountBalance.getAccount());
+            }
+            accountBalanceMap.put(accountId, accountBalance);
         }
-        accountBalance.setAmount(accountBalance.getAmount().add(amount));
-        accountBalanceRepository.save(accountBalance);
+
+        return accountBalance;
     }
 
-    private void createOperation(PaymentOrder paymentOrder) {
-        Operation operation = new Operation(
+    private Operation getOperation(PaymentOrder paymentOrder) {
+        return new Operation(
                 paymentOrder.getDate(),
                 paymentOrder.getAmount(),
                 paymentOrder.getSenderAccount(),
                 paymentOrder.getRecipientAccount(),
                 paymentOrder.getReason());
-        operationRepository.save(operation);
     }
 }
